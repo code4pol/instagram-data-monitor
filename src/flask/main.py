3505fce2e46a61d7a3f62190c740eff2a3a251e2
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-06-14 09:13:31 -0300

    endpoint lista de atores

--- src/flask/main.py
+++ src/flask/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
- 
+import re
+import json
 app = Flask(__name__)
  
 @app.route("/")
@@ -14,6 +15,16 @@ def actor_name(actor_name):
 @app.route('/actorjson/<actor_name>', methods=['GET'])
 def actor_name_json(actor_name):
     return jsonify({'actor_name' : actor_name})
+
+@app.route('/actors', methods=['GET'])
+def actor_list():
+	h = {}
+	h['actors'] = []
+	with open('../atores_lista', 'r') as f:
+		for l in f:
+			h['actors'].append(l.split('/')[-2])
+	return json.dumps(h)
+
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', debug=True)
